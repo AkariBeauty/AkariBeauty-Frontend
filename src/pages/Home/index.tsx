067c0ef781958b5355ba8c724ca44b0bafb1e8f4
Gabriel Neto Siqueira <gabriel272173siqueira@gmail.com>
2025-04-03 11:57:03 -0300

    :heavy_plus_sign: build: Atualiza estrutura do projeto, adiciona roteamento e integra Tailwind CSS.

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div className="w-screen h-screen flex flex-col justify-center items-center"> 
+            <h1 className="bg-red-500 text-white p-4 rounded">Home Page</h1>
+            <p className="bg-green-500 text-white p-2 rounded">Welcome to the home page!</p>
+        </div>
+    );
+}
